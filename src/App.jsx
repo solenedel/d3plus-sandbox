0e9cf84c1faa35f9bc233258c882bd51ed392fd4
solenedel <solene98@student.ubc.ca>
2022-02-27 20:21:59 -0800

    create dropdown component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import TreemapChart from "./Charts/d3Treemap";
+import Dropdown from "./components/Dropdown";
 
 function App() {
   return (
     <div className="App">
       <h1>d3plus sandbox</h1>
       <TreemapChart />
+      <Dropdown />
     </div>
   );
 }
---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Dropdown = () => {
+  return (
+    <select>
+      Select an option
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+      <option value="mercedes">Mercedes</option>
+      <option value="audi">Audi</option>
+    </select>
+  );
+};
+
+export default Dropdown;
